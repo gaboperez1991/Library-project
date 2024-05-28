220dd80b1c477219dc501158f168c9e4154f4dcf
gaboperez1991 <gabrielhernan-1991@outlook.com>
2024-05-27 21:42:37 -0300

    Actualizacion de CSS y ReadPage

--- src/PAGES/ReadPage.jsx
+++ src/PAGES/ReadPage.jsx
@@ -1,10 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./readpage.css"
 import { Button, Container, Table, Modal, Form } from "react-bootstrap";
 import { Pagination } from "react-bootstrap";
 import { Link } from "../Link";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { FaPlus } from "react-icons/fa";
 import { MdAddToPhotos, MdEdit, MdOutlineDeleteForever } from "react-icons/md";
 
 const ReadPage = () => {
@@ -15,33 +15,34 @@ const ReadPage = () => {
 
   const [customEntries, setCustomEntries] = useState([]);
   const [showModal, setShowModal] = useState(false);
-  const [currentEntry, setCurrentEntry] = useState({ title: "", content: "" });
+  const [currentEntry, setCurrentEntry] = useState({ description: "", year: "" , author: ""});
   const [editIndex, setEditIndex] = useState(null);
 
   useEffect(() => {
-    fetchBooks();
-  }, []);
-  const fetchBooks = async () => {
-    try {
-      const response = await axios.get(
-        "https://openlibrary.org/search.json?q=the+lord+of+the+rings"
-      );
-      setBooks(response.data.docs);
+    axios.get('https://openlibrary.org/search.json?q=the+lord+of+the+rings')
+      .then(response => {
+        const mappedBooks = response.data.docs.map(book => ({
+          year: book.first_publish_year,
+          description: book.title, 
+          author: book.author_name ? book.author_name.join(', ') : 'Desconocido'
+        }));
+        setBooks(mappedBooks);
+        setLoading(false)
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+    }, []);
 
-      setLoading(false);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      setLoading(false);
-    }
-  };
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   const indexOfLastBook = currentPage * booksPerPage;
   const indexOfFirstBook = indexOfLastBook - booksPerPage;
-  const currentBooks = books.slice(indexOfFirstBook, indexOfLastBook);
+  const allBooks = [...books, ...customEntries]
+  const currentBooks = allBooks.slice(indexOfFirstBook, indexOfLastBook);
 
   const pageNumbers = [];
-  for (let i = 1; i <= Math.ceil(books.length / booksPerPage); i++) {
+  for (let i = 1; i <= Math.ceil(allBooks.length / booksPerPage); i++) {
     pageNumbers.push(
       <Pagination.Item
         key={i}
@@ -55,20 +56,23 @@ const ReadPage = () => {
 
   const handleAdd = () => {
     setShowModal(true);
-    setCurrentEntry({ title: "", first_publish_year: "", author_name: "" });
+    setCurrentEntry({ description: "", year: "", author: ""});
     setEditIndex(null);
   };
 
   const handleEdit = (index) => {
-    setShowModal(true);
-    setCurrentEntry(customEntries[index]);
-    setEditIndex(index);
-  };
+    const apiIndex = index + indexOfFirstBook
+      setShowModal(true);
+      setCurrentEntry(allBooks[apiIndex]);
+      setEditIndex(apiIndex);
+    
+}
 
   const handleSave = () => {
-    if (editIndex !== null) {
+    if (editIndex !== null && editIndex >= books.length) {
+      const customIndex = editIndex - books.length
       const updateEntries = customEntries.map((entry, index) =>
-        index === editIndex ? currentEntry : entry
+        index === customIndex ? currentEntry : entry
       );
       setCustomEntries(updateEntries);
     } else {
@@ -78,8 +82,12 @@ const ReadPage = () => {
   };
 
   const handleDelete = (index) => {
-    setCustomEntries(customEntries.filter((_, i) => i !== index));
-  };
+    const apiIndex = index + indexOfFirstBook
+    if (apiIndex >= books.length) {
+      const customIndex = apiIndex - books.length
+      setCustomEntries(customEntries.filter((_, i) => i !== customIndex));
+    }
+    };
 
   const paginationBasic = (
     <div>
@@ -89,93 +97,60 @@ const ReadPage = () => {
   );
 
   return (
-    <div className="aling-middle juantify-content-center">
-      <Container
-        style={{
-          backgroundColor: "lightblue",
-          color: "navy",
-          padding: "20px",
-          marginTop: "20px",
-        }}
-      >
-        <h1 style={{ color: "darkblue" }}>
-          Lista de Libros El Señor de los Anillos
-        </h1>
+    <div className="align-middle justify-content-center">
+      <Container className="container-custom">
+        <h1>Lista de Libros El Señor de los Anillos</h1>
+        <Button className="btn-custom-primary" onClick={handleAdd}>
+          Agrega tu Libro <MdAddToPhotos />
+        </Button>
         {loading ? (
           <p>Cargando...</p>
         ) : (
           <form>
-            {currentBooks.map((book, index) => (
-              <Table striped bordered hover variant="dark" key={index}>
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th className="text-center">Título</th>
-                    <th className="text-center">Año de Publicación</th>
-                    <th className="text-center">Autor</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>{indexOfFirstBook + index + 1}</td>
-                    <td className="text-center">{book.title}</td>
-                    <td className="text-center">{book.first_publish_year}</td>
-                    <td className="text-center">{book.author_name}</td>
-                  </tr>
-                </tbody>
-              </Table>
-            ))}
+            <Table className="table-custom" striped bordered hover>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th className="text-center">Título</th>
+                  <th className="text-center">Año de Publicación</th>
+                  <th className="text-center">Autor</th>
+                  <th className="text-center">Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                {currentBooks.map((entry, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{indexOfFirstBook + index + 1}</td>
+                      <td className="text-center" style={{ verticalAlign: "middle" }}>
+                        {entry.description}
+                      </td>
+                      <td className="text-center" style={{ verticalAlign: "middle" }}>
+                        {entry.year}
+                      </td>
+                      <td className="text-center" style={{ verticalAlign: "middle" }}>
+                        {entry.author}
+                      </td>
+                      <td className="text-center" style={{ verticalAlign: "middle" }}>
+                        <Button className="btn-custom-primary" onClick={() => handleEdit (index)}>
+                          <MdEdit />
+                        </Button> {" "}
+                        <Button className="btn-custom-danger" onClick={() => handleDelete(index)}>
+                          <MdOutlineDeleteForever />
+                        </Button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
           </form>
         )}
-        {paginationBasic}
+        <div className="pagination-custom">
+          <Pagination>{paginationBasic}</Pagination>
+        </div>
       </Container>
-
-      <Container
-        style={{
-          backgroundColor: "lightblue",
-          color: "navy",
-          padding: "20px",
-          marginTop: "20px",
-        }}
-      >
-        <h2 style={{ color: "darkblue" }}>Agrega tu libro aquí!</h2>
-        <Button variant="primary" onClick={handleAdd}>
-          Agrega tu Libro <MdAddToPhotos />
-        </Button>
-        <Table
-          striped
-          borderer
-          hover
-          variant="dark"
-          style={{ marginTop: "20px" }}
-        >
-          <thead>
-            <tr>
-              <th>#</th>
-              <th className="text-center">Título</th>
-              <th className="text-center">Año de Publicación</th>
-              <th className="text-center">Autor</th>
-            </tr>
-          </thead>
-          <tbody>
-            {customEntries.map((entry, index) => (
-              <tr key={index}>
-                <td>{index + 1}</td>
-                <td className="text-center">{entry.title}</td>
-                <td className="text-center">{entry.first_publish_year}</td>
-                <td className="text-center">{entry.author_name}</td>
-                <Button variant="danger" onClick={() => handleEdit(index)}>
-                  <MdEdit />
-                </Button>
-                <Button variant="danger" onClick={() => handleDelete(index)}>
-                  <MdOutlineDeleteForever />
-                </Button>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </Container>
-
+  
       <Modal show={showModal} onHide={() => setShowModal(false)}>
         <Modal.Header closeButton>
           <Modal.Title>
@@ -188,25 +163,25 @@ const ReadPage = () => {
               <Form.Label>Título</Form.Label>
               <Form.Control
                 type="text"
-                value={currentEntry.title}
+                value={currentEntry.description}
                 onChange={(e) =>
-                  setCurrentEntry({ ...currentEntry, title: e.target.value})
+                  setCurrentEntry({ ...currentEntry, description: e.target.value })
                 }
               />
               <Form.Label>Año de Lanzamiento</Form.Label>
               <Form.Control
                 type="text"
-                value={currentEntry.first_publish_year}
+                value={currentEntry.year}
                 onChange={(e) =>
-                  setCurrentEntry({ ...currentEntry, first_publish_year: e.target.value})
+                  setCurrentEntry({ ...currentEntry, year: e.target.value })
                 }
               />
               <Form.Label>Autor</Form.Label>
               <Form.Control
                 type="text"
-                value={currentEntry.author_name}
+                value={currentEntry.author}
                 onChange={(e) =>
-                  setCurrentEntry({ ...currentEntry, author_name: e.target.value})
+                  setCurrentEntry({ ...currentEntry, author: e.target.value })
                 }
               />
             </Form.Group>
@@ -216,17 +191,18 @@ const ReadPage = () => {
           <Button variant="secondary" onClick={() => setShowModal(false)}>
             Cancelar
           </Button>
-          <Button variant="primary" onClick={handleSave}>
+          <Button className="btn-custom-primary" onClick={handleSave}>
             Guardar
           </Button>
         </Modal.Footer>
       </Modal>
-
+  
       <Button variant="outline-primary" style={{ marginTop: "10px" }}>
         {<Link to="/">Ir a la Home</Link>}
       </Button>
     </div>
   );
+  
 };
 
 export default ReadPage;
